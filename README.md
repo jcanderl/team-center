cb284d9f78de4f996e1dc8183c58ea59a5a21479
JC <jc@profitpeakmarketing.com>
2015-12-30 05:59:26 -0600

    commit

--- README.md
+++
@@ -1,3 +0,0 @@
-Welcome to my Team Center app
-Testing purposes only
-Test 1
---
+++ db.sql
@@ -0,0 +1,3 @@
+create table projects2 (id integer, name text);
+insert into projects2 values (1, 'project 1');
+insert into projects2 values (2, 'project 2');
